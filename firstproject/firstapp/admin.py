00afcc4291e4d63683901c0046962e33d0359b1a
Nikhil Goyal <nik.techxr@gmail.com>
2022-01-26 15:19:51 +0530

    bug remove

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Mystudent)
+admin.site.register(Myteacher)
--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
+from django.forms import NullBooleanField
 
 
+class Mystudent(models.Model):
+    name=models.CharField(max_length=50, null=True)
+    scholornum=models.IntegerField(default=0, )
+
+class Myteacher(models.Model):
+    name=models.CharField(max_length=50)
+    serialno=models.IntegerField(default=0)
+    
